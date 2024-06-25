d05660768a5daecb0d594b4deb3576ba85c7b97c
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2024-06-25 19:21:20 +0700

    first-init-app

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    files: ["**/*.js"], 
+    languageOptions: 
+      {sourceType: "commonjs"}
+  },
+  {
+    languageOptions: 
+      { globals: globals.browser }
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  }
+];
---
+++ src/routers/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+
+router.get("/", (req, res) => {
+  return res.json({
+      success: true,
+      message: "Backend is running well"
+  })
+})
+
+// router.use("/admin", require("./admin/admin.router"))
+// router.use("/auth", require("./auth.router"))
+// router.use("/profile", require("./profile.router"))
+// router.use("/item-list", require("./item/index"))
+
+
+router.use("*", (req, res) => {
+  return res.status(404).json({
+      success: false,
+      message: "Resource not found"
+  })
+})
+
+module.exports = router
